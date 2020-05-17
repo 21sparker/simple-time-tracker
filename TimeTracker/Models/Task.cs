58dd70dc8dec03de63d766d3562a5b1c1c445cb7
Sean <seanparker500@gmail.com>
2020-05-16 20:08:18 -0400

    Added Task model

---
+++ TimeTracker/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace TimeTracker
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        public string Description { get; set; }
+
+        public long CreatedDateTime { get; set; }
+
+        public long? DeletedDateTime { get; set; }
+        
+        [Computed]
+        public long? DateTracked { get; set; }
+
+        [Computed]
+        public long? SecondsTracked { get; set; }
+
+    }
+}
