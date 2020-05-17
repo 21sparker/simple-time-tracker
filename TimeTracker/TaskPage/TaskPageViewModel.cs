4bbd33ea99c5d78f01e7e1f73eb3247cbc32ee4b
Sean <seanparker500@gmail.com>
2020-05-17 17:15:22 -0400

    fix: Changed all Tasks to TaskViewModels

--- TimeTracker/TaskPage/TaskPageViewModel.cs
+++ TimeTracker/TaskPage/TaskPageViewModel.cs
@@ -14,14 +14,19 @@ namespace TimeTracker
         public string Name { get { return "Tasks"; } }
 
         private DatabaseGateway _dbGateway;
-        public ObservableCollection<Task> Tasks { get; set; }
+        public ObservableCollection<TaskViewModel> TaskViewModels { get; set; }
 
         public TaskPageViewModel(DatabaseGateway dbGateway)
         {
             _dbGateway = dbGateway;
 
             // Loads active tasks
-            Tasks = new ObservableCollection<Task>(_dbGateway.LoadTasks());
+            TaskViewModels = new ObservableCollection<TaskViewModel>();
+            List<Task> tasks = _dbGateway.LoadTasks();
+            foreach (Task task in tasks)
+            {
+                TaskViewModels.Add(new TaskViewModel(task, _dbGateway));
+            }
         }
 
         private string _taskToAdd;
@@ -62,7 +67,7 @@ namespace TimeTracker
             // TODO: Check for field that is just full of spaces
 
             // Task Description is not already uses, case-insensitive
-            if (Tasks.Any(t => t.Description.Equals(taskDescription, StringComparison.CurrentCultureIgnoreCase)))
+            if (TaskViewModels.Any(t => t.Description.Equals(taskDescription, StringComparison.CurrentCultureIgnoreCase)))
             {
                 return;
             }
@@ -71,7 +76,7 @@ namespace TimeTracker
             Task newTask = _dbGateway.InsertNewTask(taskDescription, Utilities.ConvertToUnixTime(DateTime.Now));
 
             // Update collection
-            Tasks.Add(newTask);
+            TaskViewModels.Add(new TaskViewModel(newTask, _dbGateway));
         }
 
 
@@ -82,20 +87,20 @@ namespace TimeTracker
             {
                 if (_deleteTaskCommand == null)
                 {
-                    _deleteTaskCommand = new RelayCommand(t => DeleteTask((Task)t));
+                    _deleteTaskCommand = new RelayCommand(t => DeleteTask((TaskViewModel)t));
                 }
 
                 return _deleteTaskCommand;
             }
         }
 
-        public void DeleteTask(Task task)
+        public void DeleteTask(TaskViewModel task)
         {
             // Delete task from database
-            _dbGateway.DeleteTask(task);
+            _dbGateway.DeleteTask(task.TaskItem);
 
             // Remove task from collection
-            Tasks.Remove(task);
+            TaskViewModels.Remove(task);
         }
     }
 }
