72bec83198513e684091425306ca87517c30a46e
Sean <seanparker500@gmail.com>
2020-05-17 11:09:00 -0400

    Include func to load tasks from DB

--- TimeTracker/Data/DatabaseGateway.cs
+++ TimeTracker/Data/DatabaseGateway.cs
@@ -18,5 +18,29 @@ namespace TimeTracker
         {
             _DBConnection.Close();
         }
+
+        public List<Task> LoadTasks()
+        {
+            return LoadTasks(ConvertToUnixTime(DateTime.Today));
+        }
+
+        public List<Task> LoadTasks(long dateTracked)
+        {
+            string sql =
+                "SELECT * FROM Task JOIN " +
+                "(SELECT DateTracked, TaskId, SUM(SecondsTracked) AS SecondsTracked FROM TaskHistory " +
+                "GROUP BY DateTracked, TaskId) AS TH " +
+                "ON Task.TaskId = TH.TaskId " +
+                "WHERE Task.DeletedDateTime IS NULL AND TH.DateTracked = @DateTracked;";
+            return _DBConnection.Query<Task>(sql, new { DateTracked = dateTracked }).AsList();
+        }
+
+
+        private static long ConvertToUnixTime(DateTime datetime)
+        {
+            DateTime sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            return (long)(datetime - sTime).TotalSeconds;
+        }
     }
 }
