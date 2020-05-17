e28a5fe2a836732d66a22766925d20ac6468f17f
Sean <seanparker500@gmail.com>
2020-05-17 13:02:32 -0400

    refactor: Add Utilities class

---
+++ TimeTracker/Helpers/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker
+{
+    public static class Utilities
+    {
+        public static long ConvertToUnixTime(DateTime dateTime)
+        {
+            DateTime sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return (long)(dateTime - sTime).TotalSeconds;
+        }
+    }
+}
