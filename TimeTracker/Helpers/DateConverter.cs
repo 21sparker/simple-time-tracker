f4080d16f043670027c41dbd893bd2d9e7063db1
Sean <seanparker500@gmail.com>
2020-06-20 09:54:14 -0400

    Added DateConverter for Tracked Date display

---
+++ TimeTracker/Helpers/DateConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TimeTracker
+{
+    [ValueConversion(typeof(DateTime), typeof(string))]
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateItem = (DateTime)value;
+
+            return dateItem.ToString("MMMM d, yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
+
+
---
+++ TimeTracker/Helpers/TimeDurationConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TimeTracker
+{
+    [ValueConversion(typeof(long), typeof(string))]
+    public class TimeDurationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan ts = TimeSpan.FromSeconds((long)value);
+
+            string outputString = "";
+            if (ts.Hours > 0)
+            {
+                outputString += $"{ts.Hours} hours ";
+            }
+            if (ts.Minutes > 0)
+            {
+                outputString += $"{ts.Minutes} minutes ";
+            }
+            if (ts.Seconds > 0)
+            {
+                outputString += $"{ts.Seconds} seconds";
+            }
+
+            return outputString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
